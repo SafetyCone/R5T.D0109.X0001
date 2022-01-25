c858b0e3196d7c725424e0291e57aa7e15fb6390
davidcoats <david@getsafetycone.com>
2022-01-25 15:42:09 -0800

    Initial commit of source-stub.

---
+++ source/R5T.D0109.X0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0109.X0001.Construction
+{
+    /// <summary>
+    /// Construction project for Public IExtensionMethodBaseExtensionRepositoryFilePathsProvider implementation library..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.D0109.X0001/Code/Classes/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.D0109.X0001
+{
+    public class Class1
+    {
+    }
+}
